db9a7d654aaf8a8dfe757a39c73fab07749d7d40
Prune Pillone <prune.pillone@etu.unice.fr>
2018-06-14 17:07:34 +0200

    Add component

---
+++ Front-end/src/app/declaration/issue/issue-iot/issue-iot.component.ts
@@ -0,0 +1,41 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import {IssueModel} from "../../../../shared/models/IssueModel";
+import {DialogOverviewIotComponent} from "../dialog-overview-iot/dialog-overview-iot.component";
+import {ConnectedObjectModel} from "../../../../shared/models/ConnectedObjectModel";
+import {ConnectedObjectRequestModel} from "../../../../shared/models/ConnectedObjectRequestModel";
+
+@Component({
+    selector: 'app-issue-iot',
+    templateUrl: './issue-iot.component.html',
+    styleUrls: ['./issue-iot.component.css']
+})
+export class IssueIotComponent implements OnInit {
+
+    @Input() issueIot: ConnectedObjectRequestModel;
+
+    constructor(private dialog: MatDialog) {
+    }
+
+    ngOnInit() {
+    }
+
+    openDialog(): void {
+        const dialogRef = this.dialog.open(DialogOverviewIotComponent, {
+            width: '550px',
+            data: {
+                id: this.issueIot.id,
+                title: this.issueIot.title,
+                description: this.issueIot.description,
+                emergency: this.issueIot.emergency.name,
+                area: this.issueIot.area.name,
+                type: this.issueIot.actionType,
+                date: this.issueIot.date,
+                time: this.issueIot.heure,
+                connectedObject: this.issueIot.connectedObject.name,
+                author: this.issueIot.author.firstname + " " + this.issueIot.author.lastname,
+            }
+        });
+    }
+
+}
---
+++ Front-end/src/app/declaration/issue/issue-iot/issue-iot.module.ts
@@ -0,0 +1,19 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatDialogModule} from '@angular/material/dialog';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatButtonModule} from '@angular/material/button';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterializeModule} from "angular2-materialize";
+import {DialogOverviewIotComponent} from '../dialog-overview-iot/dialog-overview-iot.component';
+import {IssueIotComponent} from './issue-iot.component';
+
+@NgModule({
+    declarations: [DialogOverviewIotComponent, IssueIotComponent],
+    imports: [CommonModule, MatDialogModule, BrowserAnimationsModule, FormsModule, ReactiveFormsModule, MaterializeModule, MatButtonModule],
+    exports: [],
+    providers: [],
+    entryComponents: []
+})
+export class IssueIotModule {
+}
